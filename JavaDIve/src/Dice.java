d5fa9a9545284adbb5ce025fa4aa35e8ca54a71c
jhs <skc05096@naver.com>
2025-01-22 19:24:01 +0900

    현식,dice ,main 수정

--- JavaDIve/src/Dice.java
+++ JavaDIve/src/Dice.java
@@ -1,4 +1,27 @@
 
-public class DIce {
+public class Dice {
+	private int diceNum = 0;
+	private static final int MIN_VALUE = 1; 
+    private static final int MAX_VALUE = 6;
+	public Dice() {
+		random();
+	}
 
+	public int getDiceNum() {
+		return diceNum;
+	}
+
+	public void setDiceNum(int diceNum) {
+		this.diceNum = diceNum;
+	}
+	
+	public void random() {
+		setDiceNum((int)(Math.random()*MAX_VALUE)+MIN_VALUE);
+	}
+	
+	public void printDice() {
+		System.out.print("주사위 숫자는:  ");
+		System.out.print(getDiceNum());
+	}
+	
 }
