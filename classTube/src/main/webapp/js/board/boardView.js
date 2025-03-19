1dd94f8826447fcca441551bb1f541b0cd1ce038
jhs <skc05096@naver.com>
2025-03-19 15:03:58 +0900

    댓글창 미세조정 /

--- classTube/src/main/webapp/js/board/boardView.js
+++ classTube/src/main/webapp/js/board/boardView.js
@@ -10,7 +10,7 @@ var contextPath = "${contextPath}"
     function openEditWindow(commentId,postId) {
   // JSP에서 컨텍스트 패스를 JavaScript 변수로 저장
         let url = contextPath + "/CommentView?commentId=" + commentId + "&postId=" + postId;
-        let popupOptions = "width=500,height=300,top=200,left=300,resizable=yes,scrollbars=yes";
+        let popupOptions = "width=600,height=300,top=200,left=300,resizable=yes,scrollbars=yes";
         window.open(url, "editComment", popupOptions);
     }
 
